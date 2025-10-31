814df0c37d7c157f3105e344cd7ccd66314aacfb
NathaliaCSd <csnathalia@estudante.ufscar.br>
2025-10-31 14:00:32 -0300

    arrumado slide principal

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
     //menu
     var windows = $(window);
     var stick = $(".header-sticky");
-    windows.on('scroll', function () {
+    windows.on('scroll', function () { 
         var scroll = windows.scrollTop();
         if (scroll < 245) {
             stick.removeClass("sticky");
